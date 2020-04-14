f62ad0924f1fee36035e786e22119f54037eee35
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-04-14 08:16:15 +0300

    fix 50

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import FriendsList from "./components/FriendList/FriendsList";
-import Profile from './components/SocialProfile/Profile';
-import Statistic from './components/Statistics/Statistic';
+import React               from 'react';
+import FriendsList         from "./components/FriendList/FriendsList";
+import Profile             from './components/SocialProfile/Profile';
+import Statistic           from './components/Statistics/Statistic';
 import TransactionsHistory from './components/TransactionHistory/TransactionsHistory';
-import friends from './data/friends';
-import user from './data/user';
-import statistic from './data/statistic';
-import transactions from './data/transactions';
+import friends             from './data/friends';
+import user                from './data/user';
+import transactions        from './data/transactions';
+import statistic           from './data/statistic';
 
 const App = () => (
     <>
-        <FriendsList friends={friends} />
+        <FriendsList friends={friends}/>
         <Profile user={user}/>
         <Statistic stats={statistic} title={'Upload stats'}/>
         <TransactionsHistory transactions={transactions}/>
