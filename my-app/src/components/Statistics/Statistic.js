853a9c94b98d97f4b41082664a3e90694a40cdbb
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-04-14 08:49:19 +0300

    fix 51

--- my-app/src/components/Statistics/Statistic.js
+++ my-app/src/components/Statistics/Statistic.js
@@ -3,22 +3,20 @@ import PropTypes     from 'prop-types';
 import styles        from './Statistic.module.css';
 import StatisticItem from './StatisticItem';
 
-class Statistic extends React.Component {
-    render() {
-        let {title, stats} = this.props;
-        return (
-            <section className={ styles.statistics }>
-                { title ? (
-                    <h2 className={ styles.title }>{ title }</h2>
-                ) : ('') }
-                <ul className={ styles.list }>
-                    { stats.map( stat => (
-                        <StatisticItem stat={ stat } key={ stat.id }/>
-                    ) ) }
-                </ul>
-            </section>
-        );
-    }
+function Statistic(props) {
+    let {title, stats} = props;
+    return (
+        <section className={styles.statistics}>
+            {title ? (
+                <h2 className={styles.title}>{title}</h2>
+            ) : ('')}
+            <ul className={styles.list}>
+                {stats.map( stat => (
+                    <StatisticItem stat={stat} key={stat.id}/>
+                ) )}
+            </ul>
+        </section>
+    );
 }
 
 Statistic.propTypes = {
--- my-app/src/components/Statistics/StatisticItem.js
+++ my-app/src/components/Statistics/StatisticItem.js
@@ -2,18 +2,16 @@ import React     from 'react';
 import PropTypes from 'prop-types';
 import styles    from "./StatisticItem.module.css";
 
-class StatisticItem extends React.Component {
-    render() {
-        let {stat} = this.props;
-        const randColor = () => "HSL(" + 360 * Math.random() + ',' + (30 + 80 * Math.random()) + '%,' + (40 + 80 * Math.random()) + '%)';
+function StatisticItem(props) {
+    let {stat} = props;
+    const randColor = () => "HSL(" + 360 * Math.random() + ',' + (30 + 80 * Math.random()) + '%,' + (40 + 70 * Math.random()) + '%)';
 
-        return (
-            <li style={{backgroundColor: randColor()}} className={styles.item}>
-                <span className={styles.label}>{stat.label}</span>
-                <span className={styles.percent}>{stat.percentage}%</span>
-            </li>
-        )
-    }
+    return (
+        <li style={{backgroundColor : randColor()}} className={styles.item}>
+            <span className={styles.label}>{stat.label}</span>
+            <span className={styles.percent}>{stat.percentage}%</span>
+        </li>
+    )
 }
 
 StatisticItem.propTypes = {
