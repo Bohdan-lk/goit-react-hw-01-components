7e822af6b3f008ee1c0e623c8386bf99d64679c7
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-04-20 13:55:15 +0300

    Updates

---
+++ precache-manifest.e1d335e7811ed85558d0b7c2e8229a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d42170aafc82ea9a45157d9a55f2f90",
+    "url": "/goit-react-hw-01-components/my-app/index.html"
+  },
+  {
+    "revision": "268145005767def7bd4b",
+    "url": "/goit-react-hw-01-components/my-app/static/css/main.bf4fa891.chunk.css"
+  },
+  {
+    "revision": "8285d3631d0e0bf70b98",
+    "url": "/goit-react-hw-01-components/my-app/static/js/2.fd6ffe5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/my-app/static/js/2.fd6ffe5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268145005767def7bd4b",
+    "url": "/goit-react-hw-01-components/my-app/static/js/main.a8a196c3.chunk.js"
+  },
+  {
+    "revision": "e9f54b97443c55a121b1",
+    "url": "/goit-react-hw-01-components/my-app/static/js/runtime-main.f2eff79f.js"
+  }
+]);
