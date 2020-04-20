52484cbe7bc1e3aac7fbe310d5aa63e0354fda8b
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-04-20 15:05:06 +0300

    Updates

---
+++ precache-manifest.58589563c0424f2576d14cd33e775165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7611f62a1e4047584ff972145a22d6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "268145005767def7bd4b",
+    "url": "/goit-react-hw-01-components/static/css/main.bf4fa891.chunk.css"
+  },
+  {
+    "revision": "8285d3631d0e0bf70b98",
+    "url": "/goit-react-hw-01-components/static/js/2.fd6ffe5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.fd6ffe5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268145005767def7bd4b",
+    "url": "/goit-react-hw-01-components/static/js/main.a8a196c3.chunk.js"
+  },
+  {
+    "revision": "601d71db586715f98a09",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.609cb800.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/my-app/precache-manifest.e1d335e7811ed85558d0b7c2e8229a2c.js"
+  "/goit-react-hw-01-components/precache-manifest.58589563c0424f2576d14cd33e775165.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
